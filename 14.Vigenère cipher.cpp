f5776d765d27f1c029536581264b6b7b7d1e3dff
CHANDRAMOHAN P <150779529+CHANDRAMOHAN338@users.noreply.github.com>
2024-07-29 12:30:41 +0530

    Add files via upload

---
+++ 14.Vigenère cipher.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double log2_factorial = 0.0;
+
+    for (int i = 1; i <= 25; i++) {
+        log2_factorial += log2(i);
+    }
+ printf("chandramohan - 192210338\n");
+	printf("Approximate number of keys for the Playfair cipher (as a power of 2): 2^%.2f\n", log2_factorial);
+
+    return 0;
+}
